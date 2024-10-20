0c4e1a7cdbd6df040f891a75f49107c4eaddb933
szholbarys <sakenzholbarys@gmail.com>
2024-10-20 22:56:56 +0500

    Initial Commit

---
+++ Xylophone/XylophoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  XylophoneApp.swift
+//  Xylophone
+//
+//  Created by Zholbarys on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct XylophoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
